6316b8ddff83f45f2d17e43af598604f01ef4722
Olayiwola Abdulrahmon <olayiwolaabdulramon96@gmail.com>
2022-09-08 15:14:29 -0700

    knock

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - the function
-* description - starting point 
-* Return :0
+*main - the function
+*description - starting point
+*return : 0
 */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}	
+}
