5c8abf58746024fcf380ad939023c1599291750e
Olayiwola Abdulrahmon <olayiwolaabdulramon96@gmail.com>
2022-09-15 14:53:43 -0700

    added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* more_numbers - prints numbers
+* Return: return nothing
+*/
+void more_numbers(void)
+{
+int ni, n2;
+for (n1 = 0; n1 < 10; n1++)
+{
+for (n2 = 0; n2 <= 14; n2++)
+{
+if (n2 > 9)
+{
+putchar((n2 / 10) + '0');
+}
+putchar((n2 % 10) + '0');
+}
+putchar(10);
+}
+}
