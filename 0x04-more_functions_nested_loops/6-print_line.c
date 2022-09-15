efb7a5c5d9278b97845a26b31d48fb4f1b58af5c
Olayiwola Abdulrahmon <olayiwolaabdulramon96@gmail.com>
2022-09-15 15:04:12 -0700

    okays

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - prints
+ * @n: parameter
+ * Return :nothing
+ */
+void print_line(int n)
+{
+while (n-- > 0)
+{
+putchar('_');
+}
+putchar('\n');
+}
