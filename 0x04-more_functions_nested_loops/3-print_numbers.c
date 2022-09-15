5cfc06fb34c1f4e0f59525424f6128c2d14b2691
Olayiwola Abdulrahmon <olayiwolaabdulramon96@gmail.com>
2022-09-15 14:22:03 -0700

    good

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_number - function that print all numbers
- * Return : returns nothing
- */
+* print_number - function that print all numbers
+* Return : returns nothing
+*/
 void print_numbers(void)
 {
 int n;
