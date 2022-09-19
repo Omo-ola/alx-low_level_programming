e47a976d6402b44a2c15bf5eb6620437e862421f
Olayiwola Abdulrahmon <olayiwolaabdulramon96@gmail.com>
2022-09-19 05:07:33 -0700

    rectified

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *reset  - checks if a character is uppercase or not
- * @n: character to be tested
- * Return: nothing
- */
+* reset  - checks if a character is uppercase or not
+* @n: character to be tested
+* Return: nothing
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
-}	
+}
