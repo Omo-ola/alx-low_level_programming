7a47f23634313e7780a14eaf3a63860cefb11fac
Olayiwola Abdulrahmon <93688539+Omo-ola@users.noreply.github.com>
2022-10-04 18:14:29 +0100

    great

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,4 @@
-nclude "main.h"
-
+#include "main.h"
 /**
  *
  *  *_strlen - count arrray
@@ -9,35 +8,16 @@ nclude "main.h"
  *    *Return: i
  *
  *     */
-
-
-
 int _strlen(char *s)
-
 {
-
-		unsigned int i;
-
-
-
-			i = 0;
-
-				while (s[i] != '\0') /*Count character of string*/
-
-						{
-
-									i++;
-
-										}
-
-
-
-					return (i);
-
+unsigned int i;
+i = 0;
+while (s[i] != '\0') /*Count character of string*/
+{
+i++;
 }
-
-
-
+return (i);
+}
 /**
  *
  *  *str_concat - back a pointer to array
@@ -49,75 +29,30 @@ int _strlen(char *s)
  *     *Return: Always an array dinamic
  *
  *      */
-
-
-
 char *str_concat(char *s1, char *s2)
-
 {
-
-		char *dst;
-
-			unsigned int i, j, size;
-
-
-
-				/*If the array is empty*/
-
-				if (s1 == NULL)
-
-							s1 = "";
-
-
-
-					if (s2 == NULL)
-
-								s2 = "";
-
-
-
-						/*count size total*/
-
-						size = (_strlen(s1) + _strlen(s2) + 1);
-
-
-
-							/*malloc*/
-
-							dst = (char *) malloc(size * sizeof(char));
-
-
-
-								if (dst == 0)
-
-										{
-
-													return (NULL);
-
-														}
-
-
-
-									/*Concatenate arrays*/
-
-									for (i = 0; *(s1 + i) != '\0'; i++)
-
-												*(dst + i) = *(s1 + i);
-
-
-
-										for (j = 0; *(s2 + j) != '\0'; j++)
-
-												{
-
-															*(dst + i) = *(s2 + j);
-
-																	i++;
-
-																		}
-
-
-
-											return (dst);
-
+char *dst;
+unsigned int i, j, size;
+/*If the array is empty*/
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+/*count size total*/
+size = (_strlen(s1) + _strlen(s2) + 1);
+/*malloc*/
+dst = (char *) malloc(size * sizeof(char));
+if (dst == 0)
+{
+return (NULL);
+}
+/*Concatenate arrays*/
+for (i = 0; *(s1 + i) != '\0'; i++)
+*(dst + i) = *(s1 + i);
+for (j = 0; *(s2 + j) != '\0'; j++)
+{
+*(dst + i) = *(s2 + j);
+i++;
+}
+return (dst);
 }
