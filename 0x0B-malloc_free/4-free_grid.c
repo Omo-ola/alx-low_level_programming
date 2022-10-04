b6732f3feb43e37a9f16ab5ce26ba432ee64c8cf
Olayiwola Abdulrahmon <93688539+Omo-ola@users.noreply.github.com>
2022-10-04 18:18:36 +0100

    added answer

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,27 +11,12 @@
  *     * Return: free grid
  *
  *      */
-
-
-
 void free_grid(int **grid, int height)
-
 {
-
-		int i;
-
-
-
-			for (i = 0; i < height; i++)
-
-					{
-
-								free(grid[i]);
-
-									}
-
-
-
-				free(grid);
-
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
 }
