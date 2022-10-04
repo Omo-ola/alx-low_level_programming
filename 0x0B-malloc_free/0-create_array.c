ab7f8d5317cb235c677f0008bd778028456522d2
Olayiwola Abdulrahmon <93688539+Omo-ola@users.noreply.github.com>
2022-10-04 19:26:14 +0100

    remove lines due to betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
-*
-*  create_array - array for prints a string
-*  @size: number elements array
+* create_array - array for prints a string
+* @size: number elements array
 * @c: char
 * Return: pointer
-* */
+*/
 char *create_array(unsigned int size, char c)
 {
 char *buffer;
